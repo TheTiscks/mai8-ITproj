beef1e27a6ac6958ea7db3deb98efe5a9b8b0ac6
TheTiscks <s-kretov@mail.ru>
2025-03-28 02:08:36 +0300

    attempt to fix

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from app.models.user import User
-from app.models.room import Room
-from app.models.booking import Booking
+
 import os   # just in case
 
 # Python 3.13   Flask 3.1.0
@@ -13,6 +11,9 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)   # создание экземпляра Flask-приложения
     db.init_app(app) # database initialisation
+    from app.models.user import User
+    from app.models.room import Room
+    from app.models.booking import Booking
 
     @app.route("/")
     def home():
