d93b1bd7001535a7a7f997c64c91407f22f02ceb
TheTiscks <thekretov@ya.ru>
2025-03-27 23:13:35 +0300

    renamed file

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import Config
@@ -6,3 +6,14 @@ from config import Config
 
 # Python 3.13   Flask 3.1.0
 
+# Инициализируем расширения
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)   # создание экземпляра Flask-приложения
+    db.init_app(app) # database initialisation
+
+    @app.route("/")
+    def home():
+        return render_template("home.html")
+    return app
--- app/config.py
+++ app/config.py
@@ -1,5 +1,10 @@
 import os
 
 class Config:
-  # configuration file
-  
+    # configuration file
+    SECRET_KEY = os.getenv("FLASK_SECRET_KEY", "dev-secret-key") # для подписи сессий и CSRF-токенов
+
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///database.db") # URI для подключения к базе данных (по умолчанию SQLite)
+    # DATABASE.DB IN ROOT PRJ FOLDER IS USED DATABASE!
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False # disable flask-sqlalchemy notifications
