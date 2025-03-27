b6714fe3025c231f821a03c37720891d46a1d8aa
TheTiscks <thekretov@ya.ru>
2025-03-27 23:35:34 +0300

    renamed file

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from config import Config
+
 #import os
 
 # Python 3.13   Flask 3.1.0
