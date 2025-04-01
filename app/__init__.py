a0be5abf19df3412f2fbd42fc12ef49dc9fcfb54
TheTiscks <s-kretov@mail.ru>
2025-04-01 21:21:40 +0300

    Фикс циклического импорта?

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from app.models.user import User
+#from app.models.user import User
 import os   # just in case
 
 # Python 3.13   Flask 3.1.0
@@ -16,7 +16,7 @@ def create_app():
     app.config.from_object("config.Config")
     db.init_app(app) # database initialisation
     login_manager.init_app(app)
-    #from app.models.user import User
+    from app.models.user import User
     from app.models.room import Room
     from app.models.booking import Booking
     from app.routes import auth_bp, main_bp
@@ -30,6 +30,6 @@ def create_app():
 
 @login_manager.user_loader
 def load_user(user_id):
-    #from app.models.user import User
+    from app.models.user import User
     return User.query.get(int(user_id))
 
